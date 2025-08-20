e6ad58ed40302fcae2d6c5f6a2fa6800ec30f2c1
waris hayat <warishayat666@gmail.com>
2025-08-20 18:54:35 +0500

    updates

--- Backend/Services/app.py
+++ Backend/Services/app.py
@@ -498,10 +498,14 @@ else:
         media_stream_constraints={"video": True, "audio": False},
         async_processing=True,
         rtc_configuration={
-        "iceServers": [
-            {"urls": ["stun:stun.l.google.com:19302"]},  # STUN
-            {"urls": ["turn:your-turn-server.com:3478"], "username": "user", "credential": "pass"}  # TURN
-        ]
-    }
+            "iceServers": [
+                {"urls": ["stun:stun.l.google.com:19302"]},
+                {
+                    "urls": ["turn:numb.viagenie.ca"],
+                    "username": "webrtc@live.com",
+                    "credential": "muazkh"
+                }
+            ]
+        }
     )
 
